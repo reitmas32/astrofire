68de8ecbf69bc089cd6e005208c400c985b87d0c
reitmas32 <rafa.zamora.rals@gmail.com>
2023-10-19 01:17:01 -0600

    feat: create  scrollControllerProvider

---
+++ lib/ui/providers/scroll_controller.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+
+final scrollControllerProvider = StateProvider((ref) => ItemScrollController());
