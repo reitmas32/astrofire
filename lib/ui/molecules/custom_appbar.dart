c95d6a4ba539cd4de23399aab439ee91173eed78
reitmas32 <rafa.zamora.rals@gmail.com>
2023-10-19 19:52:29 -0600

    feat: make responsive

--- lib/ui/molecules/custom_appbar.dart
+++ lib/ui/molecules/custom_appbar.dart
@@ -13,7 +13,7 @@ class CustomAppBar extends StatelessWidget {
 
     return Container(
       color: const Color.fromARGB(248, 30, 30, 30),
-      height: MediaQuery.of(context).size.height / 15,
+      height: 65,
       width: MediaQuery.of(context).size.width,
       padding: const EdgeInsets.only(top: 25).add(
         EdgeInsets.symmetric(
@@ -23,80 +23,85 @@ class CustomAppBar extends StatelessWidget {
               : 0,
         ),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SizedBox(
-            width: size.width > 1000 ? MediaQuery.of(context).size.width / 3 : null,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
-                  child: Image.network(
-                    'https://raw.githubusercontent.com/reitmas32/astrofire/main/assets/icon.png',
-                    height: 35,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 0),
-                  child: Text(
-                    'AstroFire',
-                    style: GoogleFonts.outfit(
-                      fontSize: 25,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: size.width > 1000
+                  ? MediaQuery.of(context).size.width / 3
+                  : null,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: Image.network(
+                      'https://raw.githubusercontent.com/reitmas32/astrofire/main/assets/icon.png',
+                      height: 35,
                     ),
                   ),
-                )
-              ],
-            ),
-          ),
-          MediaQuery.of(context).size.width > 1000
-              ? Expanded(
-                  // ignore: avoid_unnecessary_containers
-                  child: Container(
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 100),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SectionButton(
-                            lable: 'About',
-                            index: 1,
-                          ),
-                          SectionButton(
-                            lable: 'Project',
-                            index: 2,
-                          ),
-                          SectionButton(
-                            lable: 'Team',
-                            index: 3,
-                          ),
-                          SectionButton(
-                            lable: 'Contact',
-                            index: 6,
-                          ),
-                        ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 0),
+                    child: Text(
+                      'AstroFire',
+                      style: GoogleFonts.outfit(
+                        fontSize: 25,
                       ),
                     ),
-                  ),
-                )
-              : SizedBox(
-                  child: Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          Scaffold.of(context).openEndDrawer();
-                        },
-                        icon: const Icon(
-                          Icons.menu,
+                  )
+                ],
+              ),
+            ),
+            MediaQuery.of(context).size.width > 1000
+                ? Expanded(
+                    // ignore: avoid_unnecessary_containers
+                    child: Container(
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 100),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SectionButton(
+                              lable: 'About',
+                              index: 1,
+                            ),
+                            SectionButton(
+                              lable: 'Project',
+                              index: 2,
+                            ),
+                            SectionButton(
+                              lable: 'Team',
+                              index: 3,
+                            ),
+                            SectionButton(
+                              lable: 'Contact',
+                              index: 6,
+                            ),
+                          ],
                         ),
                       ),
-                      const SizedBox(
-                        width: 25,
-                      ),
-                    ],
+                    ),
+                  )
+                : SizedBox(
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            Scaffold.of(context).openEndDrawer();
+                          },
+                          icon: const Icon(
+                            Icons.menu,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 25,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-        ],
+          ],
+        ),
       ),
     );
   }
